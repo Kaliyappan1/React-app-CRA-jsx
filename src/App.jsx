00f99ce1f16f8f8399760cdab3c710e707f6eaad
kaliyappan1 <yappank17@gmail.com>
2024-05-11 14:16:49 +0530

    react props learn - default props and propes types and send props

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
-import Button from "./button";
+import Student from './student'
 function App() {
   return (
     <>
-      <Button/>
+      <Student name= "kaliyappan" age={20} student={true} contact={935905855}/>
+      <Student name= "muruganantham" age={50} student={false}/>
+      <Student name= "appu" age={15} student={true} contact="xxxxxxxxxxxx"/>
+      <Student/>
     </>
   );
 }

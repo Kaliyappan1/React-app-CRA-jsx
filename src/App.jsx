30cc58fb22271f5ddd6322486c38915fb1a6d31b
kaliyappan1 <yappank17@gmail.com>
2024-05-11 16:00:35 +0530

    validation in return format

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import LoginGreetings from "./LoginGreetings";
 function App() {
-  return <LoginGreetings Login={false} username="kaliyappan"/>
+  return <LoginGreetings Login={true} username="kaliyappan"/>
 }
 
 export default App;
--- src/LoginGreetings.jsx
+++ src/LoginGreetings.jsx
@@ -1,10 +1,12 @@
 function LoginGreetings(props) {
-    if(props.Login){
-        return <h1 className="Login">welcome to {props.username}</h1>
-    }
-    else {
-     return   <h1 className="error">please login</h1>
+    const Login = <h1 className="Login">welcome to {props.username}</h1>;
+    const error = <h1 className="error">please login</h1>;
+    
+    return(
+        <>
+       {props.Login ? Login :error}
+        </>
+    )
 }
-    }
 
 export default LoginGreetings;

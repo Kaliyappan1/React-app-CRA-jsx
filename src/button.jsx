f481f0ffb9b6576405b4ded766b504f155c2141d
kaliyappan1 <yappank17@gmail.com>
2024-05-11 13:14:50 +0530

    inline css use react

---
+++ src/button.jsx
@@ -0,0 +1,19 @@
+
+function Button() {
+    const Btn = {
+        
+            width: "200px",
+            height: "75px",
+            backgroundColor:" red",
+            color: "white",
+            fontSize: "40px",
+            border: "none",
+            borderRadius: "10px"
+}
+    return (
+        <button style={Btn}>click</button>
+        
+    )
+}
+
+export default Button;

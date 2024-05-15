1bef7d9019fdfdecafcfa127a0f2161d7118f335
kaliyappan1 <yappank17@gmail.com>
2024-05-15 16:03:20 +0530

    sample update

--- src/Addproducts.jsx
+++ src/Addproducts.jsx
@@ -1,27 +1,27 @@
 import { useState } from "react";
 
 function AddProducts() {
-    const [Fruits, setFruits] = useState(["Apple", "orange"])
+  const [Fruits, setFruits] = useState(["Apple", "orange"]);
 
-    function handleAddFruit() {
-        const newFruit = document.getElementById("foodInput").value;
-        document.getElementById("foodInput").value = "";
+  function handleAddFruit() {
+    const newFruit = document.getElementById("foodInput").value;
+    document.getElementById("foodInput").value = "";
 
-        setFruits([...Fruits, newFruit]);
-        
-    }
+    setFruits([...Fruits, newFruit]);
+  }
 
-    return (<div>
-        <h2>List of Fruid</h2>
-        <ul>
-        {Fruits.map((Fruit, index) => 
-            <li key={index}>{Fruit}</li>
-        )}
-    </ul>
-    <input type="text" id="foodInput" placeholder="Enter fruit name" />
-    <button onClick={handleAddFruit}>Add food</button>
-    </div>)
-
+  return (
+    <div>
+      <h2>List of Fruid</h2>
+      <ul>
+        {Fruits.map((Fruit, index) => (
+          <li key={index}>{Fruit}</li>
+        ))}
+      </ul>
+      <input type="text" id="foodInput" placeholder="Enter fruit name" />
+      <button onClick={handleAddFruit}>Add food</button>
+    </div>
+  );
 }
 
 export default AddProducts;
